4c4835266530d23b676992dad50d049c131db254
Sehoon In <dlstpgns0406@gmail.com>
2021-03-10 03:58:03 +0900

    refactor: fix Apps.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-
-    </div>
-  );
+class App extends Component {
+    render() {
+        return (
+            <div>
+                test
+            </div>
+        )
+    }
 }
 
 export default App;
