0515d7821bce94c68c2f1baddc64f441fa892d0c
eunjeong <poil_chxn@naver.com>
2021-03-15 21:07:12 +0900

    fix: header style

--- src/App.js
+++ src/App.js
@@ -1,30 +1,65 @@
-import React from 'react';
-import {HomePage} from 'pages';
-import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
-import SubPage from "./pages/SubPage";
+import React, { useState } from 'react';
+
+import styled from 'styled-components';
+import { HomePage } from 'pages';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import SubPage from './pages/SubPage';
+
+const MenuStyle = styled.span`
+  position: fixed;
+  z-index: 10;
+  left: 20px;
+  top: 20px;
+  color: #fff;
+  font-weight: 800;
+`;
+const ActiveMenuStyle = styled.ul`
+  position: fixed;
+  z-index: 10;
+  left: 20px;
+  top: 60px;
+  font-weight: 600;
+  font-size: 20px;
+  list-style: none;
+  padding: 0;
+  margin: 0;
+
+  a {
+    color: #fff;
+    display: inline-block;
+    text-decoration: none;
+    margin-bottom: 10px;
+  }
+`;
 
 const Nav = () => (
-    <div>
-        <ul>
-            <li><Link to="/">메인페이지</Link></li>
-            <li><Link to="/sub">서브페이지</Link></li>
-        </ul>
-    </div>
+  <ActiveMenuStyle>
+    <li>
+      <Link to="/">Ride a Bike</Link>
+    </li>
+    <li>
+      <Link to="/sub">Rabbit's World Tour</Link>
+    </li>
+  </ActiveMenuStyle>
 );
 
 function App() {
-    return (
-        <div className="App">
-            {/*<HomePage/>*/}
-            <Router>
-                <div>
-                    <Route exact path="/" component={HomePage}/>
-                    <Route path="/sub" component={SubPage}/>
-                    <Nav/>
-                </div>
-            </Router>
+  const [activeMenu, setActiveMenu] = useState(false);
+  return (
+    <div className="App">
+      {/*<HomePage/>*/}
+      <Router>
+        <div>
+          <Route exact path="/" component={HomePage} />
+          <Route path="/sub" component={SubPage} />
+          <header>
+            <MenuStyle onClick={() => setActiveMenu(!activeMenu)}>{activeMenu ? 'CLOSE' : 'MENU'}</MenuStyle>
+            {activeMenu && <Nav />}
+          </header>
         </div>
-    );
+      </Router>
+    </div>
+  );
 }
 
 export default App;
