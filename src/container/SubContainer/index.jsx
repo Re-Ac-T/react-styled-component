66877945811a3f6f7eccc528a692c5d11496743e
Sehoon In <dlstpgns0406@gmail.com>
2021-03-15 21:53:57 +0900

    refactor: fix radio-button

--- src/container/SubContainer/index.jsx
+++ src/container/SubContainer/index.jsx
@@ -8,22 +8,45 @@ import amsterdamBg from "../../assets/images/amsterdam.png";
 const SubPageLayout = styled.div`
   width: 100%;
   height: 100vh;
-  background: url(${props => props.backgroundBg ? romaBg : amsterdamBg});
+  background: url(${props => props.backgroundBg ? romaBg : amsterdamBg });
   background-size: cover;
   position: relative;
 `;
 
 const SubContainer = () => {
-    const [changeCity, setChangeCity] = useState(true);
+    const [changeCity, setChangeCity] = useState(null);
+    console.log(changeCity);
+    //
+    // const changeCityName = () => {
+    //     if(changeCity === 1){
+    //         return "romaBg"
+    //     }
+    //     else if(changeCity === 2){
+    //         return "amsterdamBg"
+    //     }
+    //
+    // }
+
     return (
         <>
-        <RadioButton changeCity={changeCity} />
-        <SubPageLayout backgroundBg={changeCity}>
-            <SubContent/>
-        </SubPageLayout>
+            <div className="md-radio md-radio-inline">
+                <input id="3" type="radio" name="g2" value="1" onClick={()=>setChangeCity(1)}  />
+                <label htmlFor="3">
+                    프랑스
+                </label>
+            </div>
+            <div className="md-radio md-radio-inline">
+                <input id="4" type="radio" name="g2" value="2" onClick={()=>setChangeCity(2)}  />
+                <label htmlFor="4">
+                    영국
+                </label>
+            </div>
+            {/*<RadioButton changeCity={changeCity} changeDayAndNight={changeDayAndNight}/>*/}
+            <SubPageLayout backgroundBg={changeCity}>
+                <SubContent/>
+            </SubPageLayout>
         </>
     );
-};
-
+}
 
 export default SubContainer;
